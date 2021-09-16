bd85e6434f7787d8e4939e4f951af6eea0670f9c
Irdjan Elezi <irdielezi@gmail.com>
2021-09-16 16:12:03 +0300

    Finished

---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,24 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+
+    public static void main(String[] args) {
+        //Ask how many users we want to add
+        System.out.print("How many you want to enroll: ");
+        Scanner in  = new Scanner(System.in);
+        int numberOfStudents = in.nextInt();
+        Student[] students = new Student[numberOfStudents];
+
+        // Create n number of new students
+        for (int i = 0; i < numberOfStudents; i++){
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+        }
+        for (int i = 0; i < numberOfStudents; i++){
+            System.out.println(students[i].toString());
+        }
+    }
+}
